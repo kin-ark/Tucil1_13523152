05bdd13ff107dfcd3d660e7f20293ba7210bd256
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-02-21 05:25:37 +0700

    feat: puzzleBoard & puzzlePiece

--- src/com/kinan/iqpuzzlerpro/game/PuzzleBoard.java
+++ src/com/kinan/iqpuzzlerpro/game/PuzzleBoard.java
@@ -1,4 +1,50 @@
 package com.kinan.iqpuzzlerpro.game;
 
 public class PuzzleBoard {
+    private int[][] board;
+    private int row, col;
+
+    public PuzzleBoard(int rows, int cols){
+        this.row = rows;
+        this.col = cols;
+        this.board = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    public boolean canPlacePiece(PuzzlePiece piece, int x, int y){
+        int[][] shape = piece.getShape();
+        for (int i = 0; i < piece.getSize(); i++){
+            int newX = x + shape[i][0];
+            int newY = y + shape[i][1];
+
+            if (newX < 0 || newY < 0 || newX >= col || newY >= row || board[newX][newY] != 0){
+                return  false;
+            }
+        }
+        return  true;
+    }
+
+    public void placePiece(PuzzlePiece piece, int x, int y){
+        int[][] shape = piece.getShape();
+        int id = piece.getId();
+        for (int i = 0; i < piece.getSize(); i++){
+            board[y + shape[i][1]][x + shape[i][0]] = id;
+        }
+    }
+
+    public boolean isSolved() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- src/com/kinan/iqpuzzlerpro/game/PuzzlePiece.java
+++ src/com/kinan/iqpuzzlerpro/game/PuzzlePiece.java
@@ -1,5 +1,53 @@
 package com.kinan.iqpuzzlerpro.game;
 
 public class PuzzlePiece {
+    private int[][] shape;
+    private int size;
+    private int id;
 
+    public PuzzlePiece(int[][] shape, int size) {
+        this.shape = shape;
+        this.size = size;
+    }
+
+    public int[][] getShape() {
+        return shape;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public PuzzlePiece rotate() {
+        int[][] newShape = new int[size][2];
+        for (int i = 0; i < size; i++) {
+            newShape[i][0] = shape[i][1];
+            newShape[i][1] = -shape[i][0];
+        }
+        return new PuzzlePiece(newShape, size);
+    }
+
+    public PuzzlePiece flipH() {
+        int[][] newShape = new int[size][2];
+        for (int i = 0; i < size; i++) {
+            newShape[i][0] = -shape[i][0];
+            newShape[i][1] = shape[i][1];
+        }
+        return new PuzzlePiece(newShape, size);
+    }
+
+    public PuzzlePiece[] getAllVariations() {
+        PuzzlePiece[] variations = new PuzzlePiece[8];
+        PuzzlePiece current = this;
+        for (int i = 0; i < 4; i++) {
+            variations[i] = current;
+            variations[i + 4] = current.flipH();
+            current = current.rotate();
+        }
+        return variations;
+    }
 }
