df39e2f5314244ab36143f968f1bd52a9c8904e9
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-02-23 23:19:10 +0700

    feat: PuzzleBoard

--- src/com/kinan/iqpuzzlerpro/game/PuzzleBoard.java
+++ src/com/kinan/iqpuzzlerpro/game/PuzzleBoard.java
@@ -1,50 +1,121 @@
 package com.kinan.iqpuzzlerpro.game;
 
-public class PuzzleBoard {
-    private int[][] board;
-    private int row, col;
+import java.util.HashMap;
+import java.util.Map;
 
-    public PuzzleBoard(int rows, int cols){
+public class PuzzleBoard {
+    private char[][] board;
+    private int row, col;
+    private Map<Character, String> pieceColors;
+    private static final String RESET = "\u001B[0m";
+
+    public PuzzleBoard(int rows, int cols) {
         this.row = rows;
         this.col = cols;
-        this.board = new int[row][col];
+        this.board = new char[row][col];
+        this.pieceColors = new HashMap<>();
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                board[i][j] = 0;
+                board[i][j] = 'X';
             }
         }
     }
 
-    public boolean canPlacePiece(PuzzlePiece piece, int x, int y){
-        int[][] shape = piece.getShape();
-        for (int i = 0; i < piece.getSize(); i++){
-            int newX = x + shape[i][0];
-            int newY = y + shape[i][1];
+    public PuzzleBoard(char[][] customBoard) {
+        this.row = customBoard.length;
+        this.col = customBoard[0].length;
+        this.board = new char[row][col];
+        this.pieceColors = new HashMap<>();
 
-            if (newX < 0 || newY < 0 || newX >= col || newY >= row || board[newX][newY] != 0){
-                return  false;
-            }
+        for (int i = 0; i < row; i++) {
+            System.arraycopy(customBoard[i], 0, board[i], 0, col);
         }
-        return  true;
     }
 
-    public void placePiece(PuzzlePiece piece, int x, int y){
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public char[][] getBoard(){
+        return board;
+    }
+
+    public boolean isEmpty(int x, int y) {
+        return board[y][x] == 'X';
+    }
+
+    public boolean canPlacePiece(PuzzlePiece piece, int x, int y) {
         int[][] shape = piece.getShape();
-        int id = piece.getId();
-        for (int i = 0; i < piece.getSize(); i++){
-            board[y + shape[i][1]][x + shape[i][0]] = id;
+    
+        for (int[] coord : shape) {
+            int newX = x + coord[0];
+            int newY = y + coord[1];
+
+            if (newX < 0 || newY < 0 || newX >= col || newY >= row || board[newY][newX] != 'X' || board[newY][newX] == '.') {
+                return false;
+            }
         }
+        return true;
+    }
+
+    public void placePiece(PuzzlePiece piece, int x, int y) {
+        int[][] shape = piece.getShape();
+        char id = piece.getId();
+    
+        for (int[] coord : shape) {
+            int newX = x + coord[0];
+            int newY = y + coord[1];
+            board[newY][newX] = id;
+        }
+
+        pieceColors.putIfAbsent(id, piece.getColor());
+    }
+
+    public void removePiece(PuzzlePiece piece, int x, int y) {
+        int[][] shape = piece.getShape();
+    
+        for (int[] coord : shape) {
+            int newX = x + coord[0];
+            int newY = y + coord[1];
+
+            if (newX >= 0 && newY >= 0 && newX < col && newY < row) {
+                board[newY][newX] = 'X';
+            }
+        }
+
+        pieceColors.remove(piece.getId());
     }
 
     public boolean isSolved() {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (board[i][j] == 0) {
+                if (board[i][j] == 'X') {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    public void printBoard() {
+        for (char[] row : board) {
+            for (char cell : row) {
+                if (cell == 'X') {
+                    System.out.print(RESET + "X  ");
+                } else if (cell == '.') {
+                    System.out.print("   " + RESET);
+                } else {
+                    String color = pieceColors.getOrDefault(cell, RESET);
+                    System.out.print(color + cell + "  " + RESET);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
